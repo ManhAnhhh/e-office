41c2b8f5d1f18601d110e0c9ee1a827bb15a597e
Mạnh Anh <142498614+ManhAnhhh@users.noreply.github.com>
2024-05-06 13:21:01 +0700

    update data

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-$(".sidebar-group").hover(
-  function () {
-    $(this).addClass("active");
-  },
-  function () {
-    $(".sidebar-group.active").removeClass("active");
-  }
-);
+$(document).ready(function () {
+  const showScrollbarNav = function () {
+    const listGroupHeight = $(".wrapper-list-group-js").innerHeight();
+    const headerHeight = $("header").innerHeight();
+    const windowHeight = $(window).innerHeight();
+    if (windowHeight <= listGroupHeight + headerHeight) {
+      $("#list-tab").addClass("overflow-y-scroll");
+    } else {
+      $("#list-tab").removeClass("overflow-y-scroll");
+    }
+  };
+  showScrollbarNav();
+
+  $(window).resize(function () {
+    showScrollbarNav();
+  });
+  $(".wrapper-list-group-js").on("click",function () {
+    showScrollbarNav();
+  });
+  $(".list-discuss-list").each(function(index, e) {
+    $(this).on("click", function() {
+      const baseUrl = window.location.origin;
+    location.href = baseUrl + `/assets/suv-html/discuss.html`;
+    })
+  })
+
+  const navList = $(".list-js"); 
+  const title = $(".title-js");
+  navList.each(function(index, e) {
+    $(this).on("click", function() {
+      title.text($(this).text());
+    })
+  })
+});

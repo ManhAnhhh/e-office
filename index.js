ecbde8965ec80465415c719f26f6f01d41ad6204
Mạnh Anh <142498614+ManhAnhhh@users.noreply.github.com>
2024-05-07 11:55:51 +0700

    update work-1-tab, copyright and fix bug navbar

--- index.js
+++ index.js
@@ -1,33 +1,17 @@
 $(document).ready(function () {
-  const showScrollbarNav = function () {
-    const listGroupHeight = $(".wrapper-list-group-js").innerHeight();
-    const headerHeight = $("header").innerHeight();
-    const windowHeight = $(window).innerHeight();
-    if (windowHeight <= listGroupHeight + headerHeight) {
-      $("#list-tab").addClass("overflow-y-scroll");
-    } else {
-      $("#list-tab").removeClass("overflow-y-scroll");
-    }
-  };
-  showScrollbarNav();
 
-  $(window).resize(function () {
-    showScrollbarNav();
-  });
-  $(".wrapper-list-group-js").on("click",function () {
-    showScrollbarNav();
-  });
-  $(".list-discuss-list").each(function(index, e) {
-    $(this).on("click", function() {
+
+  $(".list-discuss-list").each(function (index, e) {
+    $(this).on("click", function () {
       const baseUrl = window.location.origin;
-    location.href = baseUrl + `/assets/suv-html/discuss.html`;
+      location.href = baseUrl + `/assets/suv-html/discuss.html`;
     })
   })
 
-  const navList = $(".list-js"); 
+  const navList = $(".list-js");
   const title = $(".title-js");
-  navList.each(function(index, e) {
-    $(this).on("click", function() {
+  navList.each(function (index, e) {
+    $(this).on("click", function () {
       title.text($(this).text());
     })
   })
